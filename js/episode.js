8bcb21c8847073808d98301fd172b3930f8649c3
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-08-03 21:32:29 +0300

    planets

--- js/episode.js
+++ js/episode.js
@@ -35,23 +35,24 @@ function renderFilm(data) {
 
   document.querySelector('.episode-container').classList.add(data.title.toLowerCase().split(' ').join('-'))
 
-  let imagesContainer = document.createElement('div')
-  imagesContainer.classList.add('images-container')
-
   document.querySelector('.content').append(title)
   document.querySelector('.content').append(text)
+  let titlePlanets = document.createElement('h2')
+  titlePlanets.classList.add('title', 'planet-title')
+  titlePlanets.textContent = 'Planets'
+
+  for (let planet of data.planets) {
+    getData(planet).then(res => renderPlanet(res))
+  }
   for (let i = 1; i < 9; i++) {
     let image = new Image()
     image.src = `images/${data.title.toLowerCase().split(' ').join('-')}/${i}.webp`
-    image.classList.add('element-animation')
-   imagesContainer.append(image)
+    image.classList.add('episode-image', 'element-animation')
+    document.querySelector('.content').append(image)
   }
-  document.querySelector('.content').append(imagesContainer)
-  for (let planet of data.planets) {
-    getData(planet).then(res => console.log(res))
-  }
-  for (let specie of data.species) {
-    getData(specie).then(res => console.log(res))
+  document.querySelector('.content').append(titlePlanets)
+  for (let species of data.species) {
+    getData(species).then(res => console.log(res.name))
   }
   let options = { threshold: [0.5] };
   let observer = new IntersectionObserver(onEntry, options);
@@ -59,6 +60,37 @@ function renderFilm(data) {
   for (let elm of elements) {
     observer.observe(elm);
   }
+}
+function renderPlanet(planet) {
+  let planetBlock = document.createElement('div')
+  planetBlock.classList.add('planet-block')
+  let planetName = document.createElement('p')
+  planetName.classList.add('planet-descr')
+  planetName.classList.add('text')
+  planetName.innerHTML = `<span class="planet-text">Name: </span>${planet.name}`
+  let population = document.createElement('p')
+  population.classList.add('planet-descr')
+  population.classList.add('text')
+  population.innerHTML = `<span class="planet-text">Population: </span>${planet.population}`
+  let terrain = document.createElement('p')
+  terrain.classList.add('planet-descr')
+  terrain.classList.add('text')
+  terrain.innerHTML = `<span class="planet-text">Terrain: </span>${planet.terrain}`
+  let orbitalPeriod = document.createElement('p')
+  orbitalPeriod.classList.add('planet-descr')
+  orbitalPeriod.classList.add('text')
+  orbitalPeriod.innerHTML = `<span class="planet-text">Orbital Period: </span>${planet.orbital_period}`
+  let climate = document.createElement('p')
+  climate.classList.add('planet-descr')
+  climate.classList.add('text')
+  climate.innerHTML = `<span class="planet-text">Climate: </span>${planet.climate}`
+  let diameter = document.createElement('p')
+  diameter.classList.add('planet-descr')
+
+  diameter.classList.add('text')
+  diameter.innerHTML = `<span class="planet-text">Diameter: </span>${planet.diameter}`
+  for (let value of [planetName, population, terrain, orbitalPeriod, climate, diameter]) planetBlock.append(value)
+  document.querySelector('.content').append(planetBlock)
 }
 
 document.querySelectorAll("a").forEach((link) => {
